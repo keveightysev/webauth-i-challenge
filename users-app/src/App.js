4edd16690cae921dafc70d02f4e597f80e37d8ce
Kevin Smith <keveightysev@gmail.com>
2019-04-01 17:22:48 -0500

    Rudimentary react app that signs in and out and gets users list working, though it is NOT secure

--- users-app/src/App.js
+++ users-app/src/App.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
 	state = {
 		loggedIn: false,
 		username: '',
 		password: '',
+		errMessage: '',
+		users: [],
 	};
 
 	handleChanges = e => {
 		this.setState({
 			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleSubmit = async e => {
+		e.preventDefault();
+		const credentials = {
+			username: this.state.username,
+			password: this.state.password,
+		};
+		try {
+			const res = await axios.post(
+				'http://localhost:5000/users/login/',
+				credentials,
+			);
+			if (res.status === 200) {
+				const users = await axios.get('http://localhost:5000/users/users', {
+					headers: {
+						username: this.state.username,
+						password: this.state.password,
+					},
+				});
+				this.setState({
+					loggedIn: true,
+					password: '',
+					users: users.data,
+				});
+			} else {
+				this.setState({
+					errMessage: 'Invalid credentials',
+				});
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	handleSignout = e => {
+		e.preventDefault();
+		this.setState({
+			loggedIn: false,
+			username: '',
+			password: '',
+			users: [],
 		});
 	};
 
@@ -19,9 +65,12 @@ class App extends React.Component {
 				<header>
 					<h1>Users</h1>
 					{this.state.loggedIn ? (
-						<p>Welcome {this.state.username}!</p>
+						<div>
+							<p>Welcome {this.state.username}!</p>
+							<button onClick={this.handleSignout}>Sign Out</button>
+						</div>
 					) : (
-						<form>
+						<form onSubmit={this.handleSubmit}>
 							<p>Please sign in:</p>
 							<input
 								type='text'
@@ -41,6 +90,15 @@ class App extends React.Component {
 						</form>
 					)}
 				</header>
+				<main>
+					{this.state.users.length ? (
+						<div>
+							{this.state.users.map(user => (
+								<h2 key={user.id}>{user.username}</h2>
+							))}
+						</div>
+					) : null}
+				</main>
 			</div>
 		);
 	}
