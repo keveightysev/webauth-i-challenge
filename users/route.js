0932cf8586e66abd04a2857634f9fa920abc8772
Kevin Smith <keveightysev@gmail.com>
2019-04-01 16:23:00 -0500

    Restricted GET working

--- users/route.js
+++ users/route.js
@@ -44,4 +44,37 @@ router.post('/login', async (req, res) => {
 	}
 });
 
+router.get('/users', restricted, async (req, res) => {
+	try {
+		const users = await Users.find();
+		res.status(200).json(users);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'Server error' });
+	}
+});
+
+async function restricted(req, res, next) {
+	const { username, password } = req.headers;
+	if (username && password) {
+		try {
+			const user = await Users.findBy({ username });
+			if (user && bcrypt.compareSync(password, user.password)) {
+				next();
+			} else {
+				res.status(401).json({ message: 'Invalid credentials' });
+			}
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({ message: 'Server error' });
+		}
+	} else {
+		if (!username) {
+			res.status(406).json({ message: 'Please include a username' });
+		} else if (!password) {
+			res.status(406).json({ message: 'Password field blank' });
+		}
+	}
+}
+
 module.exports = router;
