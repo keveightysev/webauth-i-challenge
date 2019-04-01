6a87daba3af9fdcc07e07b680d2e1c21de743bb3
Kevin Smith <keveightysev@gmail.com>
2019-04-01 15:52:50 -0500

    Created basic db/table

---
+++ data/migrations/20190401155101_inital_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('users', table => {
+		table.increments();
+		table
+			.string('username', 50)
+			.notNullable()
+			.unique();
+		table.string('password', 128).notNullable();
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('users');
+};
